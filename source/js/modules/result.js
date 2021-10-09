e639be332d9c316675ebbbf5a2f804ed275d14e2
Ravil <tuychin@gmail.com>
2021-10-09 15:34:06 +0300

    feat(result): victory text animation

--- source/js/modules/result.js
+++ source/js/modules/result.js
@@ -1,26 +1,37 @@
+
 export default () => {
-  let showResultEls = document.querySelectorAll(`.js-show-result`);
-  let results = document.querySelectorAll(`.screen--result`);
+  const showResultEls = document.querySelectorAll(`.js-show-result`);
+  const results = Array.from(document.querySelectorAll(`.screen--result`));
+
   if (results.length) {
-    for (let i = 0; i < showResultEls.length; i++) {
-      showResultEls[i].addEventListener(`click`, function () {
-        let target = showResultEls[i].getAttribute(`data-target`);
-        [].slice.call(results).forEach(function (el) {
+    showResultEls.forEach((result) => {
+      result.addEventListener(`click`, () => {
+        const targetId = result.getAttribute(`data-target`);
+
+        results.forEach((el) => {
           el.classList.remove(`screen--show`);
           el.classList.add(`screen--hidden`);
         });
-        let targetEl = [].slice.call(results).filter(function (el) {
-          return el.getAttribute(`id`) === target;
-        });
-        targetEl[0].classList.add(`screen--show`);
-        targetEl[0].classList.remove(`screen--hidden`);
-      });
-    }
 
-    let playBtn = document.querySelector(`.js-play`);
+        const targetEl = results.find((el) => el.getAttribute(`id`) === targetId);
+
+        targetEl.classList.add(`screen--show`);
+        targetEl.classList.remove(`screen--hidden`);
+
+        targetEl.querySelectorAll(`.result__title path`).forEach((element) => {
+          const length = element.getTotalLength();
+          element.setAttribute(`stroke-dashoffset`, length);
+          element.setAttribute(`stroke-dasharray`, length);
+        });
+
+        targetEl.querySelector(`.result__title path animate`).beginElement();
+      });
+    });
+
+    const playBtn = document.querySelector(`.js-play`);
     if (playBtn) {
       playBtn.addEventListener(`click`, function () {
-        [].slice.call(results).forEach(function (el) {
+        results.forEach((el) => {
           el.classList.remove(`screen--show`);
           el.classList.add(`screen--hidden`);
         });
