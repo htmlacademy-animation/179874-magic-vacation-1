d27e12e1960ef8c692590692d4cbcb59f21b4365
Ravil <tuychin@gmail.com>
2021-10-15 03:38:13 +0300

    fix(win-scene): paths

--- source/js/canvas-scenes/win-scene.js
+++ source/js/canvas-scenes/win-scene.js
@@ -4,12 +4,12 @@ import easings from '../utils/easings.js';
 
 
 const IMAGES_URLS = Object.freeze({
-  plane: `../../img/airplane.png`,
-  tree: `../../img/tree.png`,
-  tree2: `../../img/tree-2.png`,
-  ice: `../../img/ice.png`,
-  seaCalf: `../../img/sea-calf-2.png`,
-  snowflake: `../../img/snowflake.png`
+  plane: `./img/airplane.png`,
+  tree: `./img/tree.png`,
+  tree2: `./img/tree-2.png`,
+  ice: `./img/ice.png`,
+  seaCalf: `./img/sea-calf-2.png`,
+  snowflake: `./img/snowflake.png`
 });
 
 
