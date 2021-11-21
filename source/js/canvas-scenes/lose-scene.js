69bc1b5ad811a079e43e7ca9e86379024d303e31
Ravil <tuychin@gmail.com>
2021-11-21 16:20:10 +0300

    refactor(lose-scene): key mask

--- source/js/canvas-scenes/lose-scene.js
+++ source/js/canvas-scenes/lose-scene.js
@@ -2,44 +2,15 @@ import Animation from '../utils/animation';
 import Scene2D from '../utils/scene-2d.js';
 import easings from '../utils/easings.js';
 
-
 const IMAGES_URLS = Object.freeze({
-  crocodile: `./img/module-4/lose-images/crocodile.png`,
-  drop: `./img/module-4/lose-images/drop.png`,
-  flamingo: `./img/module-4/lose-images/flamingo.png`,
   key: `./img/module-4/lose-images/key.png`,
-  leaf: `./img/module-4/lose-images/leaf.png`,
-  saturn: `./img/module-4/lose-images/saturn.png`,
-  snowflake: `./img/module-4/lose-images/snowflake.png`,
+  crocodile: `./img/module-4/lose-images/crocodile.png`,
+  flamingo: `./img/module-4/lose-images/flamingo.png`,
   watermelon: `./img/module-4/lose-images/watermelon.png`,
-});
-
-const LOCALS = Object.freeze({
-  mask: {
-    centerX: 50,
-    centerY: 43.5,
-    radius: 9.1,
-    endX: 87,
-    endY: 53,
-    startAngle: 270,
-    endAngle: 48,
-    opacity: 1,
-    color: `#5f458c`,
-    path: {
-      x1: 56.1,
-      y1: 50.3,
-      x2: 59.1,
-      y2: 65.5,
-      x3: 59.1,
-      y3: 80,
-      x4: 100,
-      y4: 80,
-      x5: 100,
-      y5: 34.45,
-      x6: 50,
-      y6: 34.45
-    }
-  }
+  leaf: `./img/module-4/lose-images/leaf.png`,
+  snowflake: `./img/module-4/lose-images/snowflake.png`,
+  saturn: `./img/module-4/lose-images/saturn.png`,
+  drop: `./img/module-4/lose-images/drop.png`,
 });
 
 const OBJECTS = Object.freeze({
@@ -47,135 +18,123 @@ const OBJECTS = Object.freeze({
     imageId: `key`,
     x: 50,
     y: 50,
-    size: 18,
+    size: 22,
     opacity: 0,
     transforms: {
-      scaleX: 0.8,
-      scaleY: 0.8
+      translateY: 0,
     }
   },
   crocodile: {
     imageId: `crocodile`,
-    x: 52,
-    y: 57,
-    size: 57,
-    opacity: 0,
+    x: 50,
+    y: 55,
+    size: 80,
     transforms: {
-      translateX: 23,
-      translateY: -12,
-      rotate: 15
+      translateX: 200,
+      translateY: 0,
     }
   },
   flamingo: {
     imageId: `flamingo`,
-    x: 50,
+    x: 55,
     y: 50,
-    size: 10,
-    opacity: 1,
+    size: 15,
+    opacity: 0,
     transforms: {
       scaleX: 0,
       scaleY: 0,
-      translateX: 0,
-      translateY: 0,
-      rotate: 60
     }
   },
   watermelon: {
     imageId: `watermelon`,
-    x: 49,
-    y: 50,
-    size: 9,
-    opacity: 1,
+    x: 55,
+    y: 55,
+    size: 15,
+    opacity: 0,
     transforms: {
       scaleX: 0,
       scaleY: 0,
-      translateX: 0,
-      translateY: 0,
-      rotate: 60
     }
   },
   leaf: {
     imageId: `leaf`,
-    x: 50,
-    y: 49,
-    size: 9,
-    opacity: 1,
+    x: 55,
+    y: 50,
+    size: 15,
+    opacity: 0,
     transforms: {
       scaleX: 0,
       scaleY: 0,
-      translateX: 0,
-      translateY: 0,
-      rotate: -40
     }
   },
   snowflake: {
     imageId: `snowflake`,
-    x: 50,
-    y: 50,
-    size: 7,
-    opacity: 1,
+    x: 55,
+    y: 55,
+    size: 15,
+    opacity: 0,
     transforms: {
       scaleX: 0,
       scaleY: 0,
-      translateX: 0,
-      translateY: 0,
-      rotate: -60
     }
   },
   saturn: {
     imageId: `saturn`,
-    x: 50,
-    y: 50,
-    size: 13,
-    opacity: 1,
+    x: 55,
+    y: 55,
+    size: 15,
+    opacity: 0,
     transforms: {
       scaleX: 0,
       scaleY: 0,
-      translateX: 0,
-      translateY: 0,
-      rotate: 50
     }
   },
   drop: {
     imageId: `drop`,
-    x: 50,
-    y: 60,
-    size: 2.5,
-    opacity: 1,
+    x: 47.5,
+    y: 58,
+    size: 3,
+    opacity: 0,
     transforms: {
-      scaleX: 0,
-      scaleY: 0
+      scaleX: 1,
+      scaleY: 0,
     }
+  },
+});
+
+const LOCALS = Object.freeze({
+  keyMask: {
+    centerX: 50,
+    centerY: 50,
   }
 });
 
-class LoseScene extends Scene2D {
+export default class CrocodileScene extends Scene2D {
   constructor() {
     const canvas = document.getElementById(`lose-scene-canvas`);
 
     super({
       canvas,
       objects: OBJECTS,
-      locals: LOCALS,
-      imagesUrls: IMAGES_URLS
+      imagesUrls: IMAGES_URLS,
     });
 
     this.afterInit = () => {
-      this.objects.flamingo.before = this.drawMask.bind(this);
+      this.objects.crocodile.after = this.drawKeyMask.bind(this);
     };
 
-    this.animations = [];
-    this.dropAnimations = [];
-    this.initObjects();
-    this.initAnimations();
+    this.initEventListeners();
+    this.initObjects(OBJECTS);
     this.initLocals();
-
-    this.animationDelay = 0;
+    this.start();
   }
 
   initLocals() {
     this.locals = {
-      ...LOCALS
+      keyMask: {
+        centerX: LOCALS.keyMask.centerX,
+        centerY: LOCALS.keyMask.centerY
+      }
     };
   }
 
@@ -189,199 +148,185 @@ class LoseScene extends Scene2D {
     }));
 
     this.initKeyAnimations();
-    this.initCrocodileAnimation();
     this.initFlamingoAnimations();
-    this.initWatermelonAnimation();
-    this.initLeafAnimation();
-    this.initSnowflakeAnimation();
-    this.initSaturnAnimation();
+    this.initWatermelonAnimations();
+    this.initLeafAnimations();
+    this.initSnowflakeAnimations();
+    this.initSaturnAnimations();
+    this.initCrocodileAnimations();
+    this.initDropAnimations();
+  }
 
-    setTimeout(() => {
-      this.initDropAnimation();
-    }, 800);
+  drawKeyMask() {
+    const mask = this.locals.keyMask;
+    const width = this.objects.key.size;
+    const size = this.size / 100;
 
-    this.animations.forEach((animation) => {
-      animation.start();
-    });
+    this.ctx.save();
+    this.ctx.fillStyle = `#5f458c`;
+
+    this.ctx.beginPath();
+
+    this.ctx.moveTo((mask.centerX + width * 3) * size, (mask.centerY + width * 0.85) * size);
+    this.ctx.lineTo((mask.centerX + width * 0.5) * size, (mask.centerY + width * 0.85) * size);
+    this.ctx.lineTo((mask.centerX + width * 0.33) * size, (mask.centerY + width * 0.005) * size);
+    this.ctx.bezierCurveTo((mask.centerX + width * 0.8) * size, (mask.centerY - width * 0.5) * size, (mask.centerX + width * 0.1) * size, (mask.centerY - width) * size, (mask.centerX + width * 0.2) * size, (mask.centerY - width) * size);
+    this.ctx.lineTo((mask.centerX + width * 3) * size, (mask.centerY - width) * size);
+
+    this.ctx.fill();
+    this.ctx.restore();
   }
 
   initKeyAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
-        const {transforms} = this.objects.key;
         this.objects.key.opacity = progress;
-        transforms.scaleX = progress > transforms.scaleX ? progress : transforms.scaleX;
-        transforms.scaleY = progress > transforms.scaleY ? progress : transforms.scaleY;
       },
-      duration: 200,
-      delay: 0,
-      easing: easings.easeOutExpo
+      duration: 1000,
+      easing: easings.easeInCubic
     }));
   }
 
   initFlamingoAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
-        const progressReversed = 1 - progress;
-        const {transforms} = this.objects.flamingo;
-
-        transforms.scaleX = progress;
-        transforms.scaleY = progress;
-        transforms.translateX = -15 * progress;
-        transforms.translateY = -5.2 * progress;
-        transforms.rotate = 60 * Math.sin(progressReversed * 2);
+        this.objects.flamingo.opacity = progress;
+        this.objects.flamingo.transforms.scaleX = progress;
+        this.objects.flamingo.transforms.scaleY = progress;
+        this.objects.flamingo.transforms.translateX = -30 * progress;
+        this.objects.flamingo.transforms.translateY = -10 * progress;
       },
-      duration: 617,
-      delay: 0,
+      delay: 1000,
+      duration: 1000,
+      easing: easings.easeInCubic
+    }));
+
+    this.animations.push(new Animation({
+      func: (progress) => {
+        this.objects.flamingo.transforms.translateY = 100 * progress;
+      },
+      delay: 2250,
+      duration: 1500,
       easing: easings.easeOutExpo
     }));
-
-    this.animations.push(new Animation({
-      func: (progress) => {
-        const {transforms} = this.objects.flamingo;
-
-        transforms.translateY = 70 * progress;
-      },
-      duration: 583,
-      delay: 617,
-      easing: easings.easeOutCubic
-    }));
   }
 
-  initWatermelonAnimation() {
+  initWatermelonAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
-        const progressReversed = 1 - progress;
-        const {transforms} = this.objects.watermelon;
-
-        transforms.scaleX = progress;
-        transforms.scaleY = progress;
-        transforms.translateX = -22 * progress;
-        transforms.translateY = 16 * progress;
-        transforms.rotate = 60 * Math.sin(progressReversed * 2);
+        this.objects.watermelon.opacity = progress;
+        this.objects.watermelon.transforms.scaleX = progress;
+        this.objects.watermelon.transforms.scaleY = progress;
+        this.objects.watermelon.transforms.translateX = -30 * progress;
+        this.objects.watermelon.transforms.translateY = 10 * progress;
       },
-      duration: 533,
-      delay: 0,
+      delay: 1000,
+      duration: 1000,
+      easing: easings.easeInCubic
+    }));
+
+    this.animations.push(new Animation({
+      func: (progress) => {
+        this.objects.watermelon.transforms.translateY = 100 * progress;
+      },
+      delay: 2250,
+      duration: 1500,
       easing: easings.easeOutExpo
     }));
-
-    this.animations.push(new Animation({
-      func: (progress) => {
-        const {transforms} = this.objects.watermelon;
-
-        transforms.translateY = 55 * progress;
-      },
-      duration: 200,
-      delay: 533,
-      easing: easings.easeOutCubic
-    }));
   }
 
-  initLeafAnimation() {
+  initLeafAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
-        const progressReversed = 1 - progress;
-        const {transforms} = this.objects.leaf;
-
-        transforms.scaleX = progress;
-        transforms.scaleY = progress;
-        transforms.translateX = 25 * progress;
-        transforms.translateY = -15 * progress;
-        transforms.rotate = -40 * Math.sin(progressReversed * 2);
+        this.objects.leaf.opacity = progress;
+        this.objects.leaf.transforms.scaleX = progress;
+        this.objects.leaf.transforms.scaleY = progress;
+        this.objects.leaf.transforms.translateX = 30 * progress;
+        this.objects.leaf.transforms.translateY = -10 * progress;
       },
-      duration: 533,
-      delay: 0,
+      delay: 1000,
+      duration: 1000,
+      easing: easings.easeInCubic
+    }));
+
+    this.animations.push(new Animation({
+      func: (progress) => {
+        this.objects.leaf.transforms.translateY = 100 * progress;
+      },
+      delay: 2250,
+      duration: 1500,
       easing: easings.easeOutExpo
     }));
-
-    this.animations.push(new Animation({
-      func: (progress) => {
-        const {transforms} = this.objects.leaf;
-
-        transforms.translateY = 78 * progress;
-      },
-      duration: 583,
-      delay: 533,
-      easing: easings.easeOutCubic
-    }));
   }
 
-  initSnowflakeAnimation() {
+  initSnowflakeAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
-        const progressReversed = 1 - progress;
-        const {transforms} = this.objects.snowflake;
-
-        transforms.scaleX = progress;
-        transforms.scaleY = progress;
-        transforms.translateX = 12 * progress;
-        transforms.translateY = 3 * progress;
-        transforms.rotate = -60 * Math.sin(progressReversed * 2);
+        this.objects.snowflake.opacity = progress;
+        this.objects.snowflake.transforms.scaleX = progress;
+        this.objects.snowflake.transforms.scaleY = progress;
+        this.objects.snowflake.transforms.translateX = 15 * progress;
+        this.objects.snowflake.transforms.translateY = -5 * progress;
       },
-      duration: 683,
-      delay: 0,
+      delay: 1000,
+      duration: 1000,
+      easing: easings.easeInCubic
+    }));
+
+    this.animations.push(new Animation({
+      func: (progress) => {
+        this.objects.snowflake.transforms.translateY = 100 * progress;
+      },
+      delay: 2250,
+      duration: 1500,
       easing: easings.easeOutExpo
     }));
-
-    this.animations.push(new Animation({
-      func: (progress) => {
-        const {transforms} = this.objects.snowflake;
-
-        transforms.translateY = 62 * progress;
-      },
-      duration: 583,
-      delay: 683,
-      easing: easings.easeOutCubic
-    }));
   }
 
-  initSaturnAnimation() {
+  initSaturnAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
-        const progressReversed = 1 - progress;
-        const {transforms} = this.objects.saturn;
-
-        transforms.scaleX = progress;
-        transforms.scaleY = progress;
-        transforms.translateX = 23 * progress;
-        transforms.translateY = 22 * progress;
-        transforms.rotate = 50 * Math.sin(progressReversed * 2);
+        this.objects.saturn.opacity = progress;
+        this.objects.saturn.transforms.scaleX = progress;
+        this.objects.saturn.transforms.scaleY = progress;
+        this.objects.saturn.transforms.translateX = 30 * progress;
+        this.objects.saturn.transforms.translateY = 10 * progress;
       },
-      duration: 617,
-      delay: 0,
+      delay: 1000,
+      duration: 1000,
+      easing: easings.easeInCubic
+    }));
+
+    this.animations.push(new Animation({
+      func: (progress) => {
+        this.objects.saturn.transforms.translateY = 100 * progress;
+      },
+      delay: 2250,
+      duration: 1500,
       easing: easings.easeOutExpo
     }));
-    this.animations.push(new Animation({
-      func: (progress) => {
-        const {transforms} = this.objects.saturn;
-
-        transforms.translateY = 55 * progress;
-      },
-      duration: 300,
-      delay: 617,
-      easing: easings.easeOutCubic
-    }));
   }
 
-  initCrocodileAnimation() {
+  initCrocodileAnimations() {
     this.animations.push(new Animation({
       func: (progress) => {
         const progressReversed = 1 - progress;
-        const {transforms} = this.objects.crocodile;
 
-        this.objects.crocodile.opacity = 1;
-        transforms.translateX = 20 * progressReversed;
-        transforms.translateY = -12 * progressReversed;
-        transforms.rotate = 15 * Math.sin(progressReversed * 2);
+        this.objects.crocodile.transforms.translateX = 50 * progressReversed;
+        this.objects.crocodile.transforms.translateY = -10 * progressReversed;
       },
-      duration: 600,
-      delay: 300,
-      easing: easings.easeLinear
+      delay: 1750,
+      duration: 1000,
+      easing: easings.easeInCubic
     }));
   }
 
-  initDropAnimation() {
+  initDropAnimations() {
+    const delayStart = 3000;
+    const interval = 2500;
+
     this.dropAnimations = [];
+
     this.dropAnimations.push(new Animation({
       func: (progress) => {
         const {transforms} = this.objects.drop;
@@ -393,6 +338,7 @@ class LoseScene extends Scene2D {
       duration: 600,
       delay: 0
     }));
+
     this.dropAnimations.push(new Animation({
       func: (progress) => {
         const {transforms} = this.objects.drop;
@@ -401,6 +347,7 @@ class LoseScene extends Scene2D {
       duration: 500,
       delay: 600,
     }));
+
     this.dropAnimations.push(new Animation({
       func: (progress) => {
         const {transforms} = this.objects.drop;
@@ -423,63 +370,14 @@ class LoseScene extends Scene2D {
       delay: 1300,
     }));
 
-    this.dropAnimations.forEach((animation) => {
-      animation.start();
-    });
+    setTimeout(() => {
+      this.dropAnimations.forEach((animation) => {
+        animation.start();
+      });
+    }, delayStart);
 
     setTimeout(() => {
-      this.initDropAnimation();
-    }, 2500);
-  }
-
-  drawMask() {
-    const b = this.locals.mask;
-    const path = b.path;
-    const s = this.size / 100;
-
-    this.ctx.save();
-    this.ctx.globalAlpha = b.opacity;
-    this.ctx.fillStyle = b.color;
-
-    this.ctx.beginPath();
-
-    this.ctx.arc(
-        b.centerX * s,
-        b.centerY * s,
-        b.radius * s,
-        (Math.PI / 180) * b.startAngle,
-        (Math.PI / 180) * b.endAngle,
-    );
-
-    this.ctx.moveTo(
-        path.x1 * s,
-        path.y1 * s
-    );
-
-    this.ctx.lineTo(
-        path.x2 * s,
-        path.y2 * s
-    );
-    this.ctx.lineTo(
-        path.x3 * s,
-        path.y3 * s
-    );
-    this.ctx.lineTo(
-        path.x4 * s,
-        path.y4 * s
-    );
-    this.ctx.lineTo(
-        path.x5 * s,
-        path.y5 * s
-    );
-    this.ctx.lineTo(
-        path.x6 * s,
-        path.y6 * s
-    );
-
-    this.ctx.fill();
-    this.ctx.restore();
+      this.initDropAnimations();
+    }, interval);
   }
 }
-
-export default LoseScene;
