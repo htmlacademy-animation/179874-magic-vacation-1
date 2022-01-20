956de4a6ba3f13b26cefb5888d9280d5ff530e18
Ravil Tuychin <tuychin@gmail.com>
2022-01-20 03:00:14 +0300

    feat(js): added 3d slider-scene

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,7 +1,9 @@
 import Swiper from "swiper";
-
+import SliderScene from '../3d-scenes/slider-scene';
 
 export default () => {
+  const sliderScene = new SliderScene();
+
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
 
@@ -48,8 +50,6 @@ export default () => {
   };
 
   const setSlider = function () {
-    // sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
-
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
       storySlider = new Swiper(`.js-slider`, {
         pagination: {
@@ -61,15 +61,15 @@ export default () => {
         },
         on: {
           slideChange: () => {
-            // if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
-            // } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
-            // } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
-            // } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
-            // }
+            if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
+              sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+            } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
+              sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
+            } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
+              sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
+            } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
+              sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
+            }
           },
           resize: () => {
             storySlider.update();
@@ -95,15 +95,15 @@ export default () => {
         },
         on: {
           slideChange: () => {
-            // if (storySlider.activeIndex === 0) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
-            // } else if (storySlider.activeIndex === 2) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
-            // } else if (storySlider.activeIndex === 4) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
-            // } else if (storySlider.activeIndex === 6) {
-            //   sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
-            // }
+            if (storySlider.activeIndex === 0) {
+              sliderScene.setBackground(0);
+            } else if (storySlider.activeIndex === 2) {
+              sliderScene.setBackground(1);
+            } else if (storySlider.activeIndex === 4) {
+              sliderScene.setBackground(2);
+            } else if (storySlider.activeIndex === 6) {
+              sliderScene.setBackground(3);
+            }
 
             emitSliderChangeEvent(storySlider.activeIndex);
           },
@@ -118,10 +118,17 @@ export default () => {
   };
 
   document.body.addEventListener(`screenChanged`, (evt) => {
+    const {screenName} = evt.detail;
     const isFirstSlide = storySlider.activeIndex === 0;
 
     if (!isFirstSlide) {
       resetSlider();
+    }
+
+    if (screenName === `story`) {
+      sliderScene.start();
+    } else {
+      sliderScene.stop();
     }
   });
 
